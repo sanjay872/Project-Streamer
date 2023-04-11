1a1447709a4f4a8a451dd5b75164ea7b86b1b40f
sanjay <rockysanjay287@gmail.com>
2023-04-11 23:37:22 +0530

    added movies service

---
+++ Services/movies-service/src/main/java/com/projectstreamer/moviesservice/MoviesServiceApplication.java
@@ -0,0 +1,14 @@
+package com.projectstreamer.moviesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class MoviesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesServiceApplication.class, args);
+	}
+
+}
